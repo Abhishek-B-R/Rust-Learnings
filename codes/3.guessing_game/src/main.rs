21549f8f8e80862f39e6d1cc9c87783e9352d5a0
Abhishek-B-R <byteblaster1468@gmail.com>
2025-10-23 01:29:27 +0530

    final code with a loop, error handling, etc done for guessing_game

--- codes/3.guessing_game/src/main.rs
+++ codes/3.guessing_game/src/main.rs
@@ -5,23 +5,29 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-    println!("Enter a number");
-
-    let mut guess = String::new();
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is {secret_number}");
     
-    stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read nuber");
+    loop {
+        println!("Enter a number");
 
-    let guess : i32 = guess.trim().parse().expect("Please enter a number"); 
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        let mut guess = String::new();
+        
+        stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read number");
+
+        let guess : i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
-    
-    println!("Guessed number is {guess}");
 }
