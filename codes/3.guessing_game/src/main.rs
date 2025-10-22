ad2d457bb7aa7fddf76e73f6b66d2feeaa61c1fe
Abhishek-B-R <byteblaster1468@gmail.com>
2025-10-23 00:09:04 +0530

    added random number generation

--- codes/3.guessing_game/src/main.rs
+++ codes/3.guessing_game/src/main.rs
@@ -1,14 +1,19 @@
 use std::io::stdin;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
     println!("Enter a number");
 
     let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is {secret_number}");
 
     stdin()
         .read_line(&mut guess)
         .expect("Failed to read nuber");
 
     println!("Guessed number is {guess}");
+    
 }
