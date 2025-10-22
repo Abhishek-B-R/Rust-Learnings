7504902224616c23ee134df2de6c4d63dadaeb2c
Abhishek-B-R <byteblaster1468@gmail.com>
2025-10-23 00:36:04 +0530

    added guess and secret comparison logic

--- codes/3.guessing_game/src/main.rs
+++ codes/3.guessing_game/src/main.rs
@@ -1,4 +1,6 @@
 use std::io::stdin;
+use std::cmp::Ordering;
+
 use rand::Rng;
 
 fn main() {
@@ -9,11 +11,17 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is {secret_number}");
-
-    stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read nuber");
-
-    println!("Guessed number is {guess}");
     
+    stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read nuber");
+
+    let guess : i32 = guess.trim().parse().expect("Please enter a number"); 
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
+    
+    println!("Guessed number is {guess}");
 }
