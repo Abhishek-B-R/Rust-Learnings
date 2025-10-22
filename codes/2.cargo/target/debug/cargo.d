ff0e57ffac71440c8f29fb6b1a80d12defbcf69e
Abhishek-B-R <byteblaster1468@gmail.com>
2025-10-22 16:08:19 +0530

    cargo config

---
+++ codes/2.cargo/target/debug/cargo.d
@@ -0,0 +1,1 @@
+/home/abhishek/Blockchain_Web3/100daysofcode/Rust/codes/2.cargo/target/debug/cargo: /home/abhishek/Blockchain_Web3/100daysofcode/Rust/codes/2.cargo/src/main.rs
---
+++ codes/2.cargo/target/debug/deps/cargo-a24a2665becc8293.d
@@ -0,0 +1,5 @@
+/home/abhishek/Blockchain_Web3/100daysofcode/Rust/codes/2.cargo/target/debug/deps/cargo-a24a2665becc8293.d: src/main.rs
+
+/home/abhishek/Blockchain_Web3/100daysofcode/Rust/codes/2.cargo/target/debug/deps/libcargo-a24a2665becc8293.rmeta: src/main.rs
+
+src/main.rs:
