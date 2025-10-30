b6a3fa846d8f2c9af2b36689d7ef944dc9a87d3c
Abhishek-B-R <byteblaster1468@gmail.com>
2025-10-30 23:21:48 +0530

    slice type and slicing in rust

---
+++ codes/9.slice_type/src/main.rs
@@ -0,0 +1,30 @@
+// fn main() {
+//     let mut s = String::from("hello there");
+//     let first = first_word(&s); // borrowing and recieve first work
+
+//     // s.clear(); // Gives error
+//     println!("{}", first); //print it
+// }
+
+// fn first_word(s : &str) -> &str{ // take immutable reference of string and return string
+//     let bytes = s.as_bytes(); // split it into bytes array
+
+//     for (i, &byte) in bytes.iter().enumerate() { // iterate the bytes and enumarate it -> gives index and 
+//         if byte == b' ' { // here b' ' means the bytecode of space(' ')
+//             return &s[0..i]; // return the first part of the string, bia reference obv
+//         }
+//     }
+
+//     return &s[..]; // if no space found, return entire s
+// }
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+
+    
+
+}
