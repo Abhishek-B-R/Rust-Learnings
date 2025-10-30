7413aff76e1ae005018504dcb323b98dbb072b1e
Abhishek-B-R <byteblaster1468@gmail.com>
2025-10-30 13:38:55 +0530

    fixed borrow checker code

--- codes/8.borrowing/src/main.rs
+++ codes/8.borrowing/src/main.rs
@@ -1,24 +1,24 @@
-// fn main() {
-//     let mut x = String::from("hello");
-//     let mut y = &x;
-
-//     let binding = String::from("hey there");
-//     y = &binding;
-//     change_value(&mut x);
-    
-//     println!("Here, x is {} and y is {}", x, y);
-// }
-
-// fn change_value(s : &mut String){
-//     s.push_str(" world");
-// }
-
 fn main() {
-    let _reference_to_nothing = dangle();
+    let mut x = String::from("hello");
+    let mut y = &x;
+
+    let binding = String::from("hey there");
+    y = &binding;
+    change_value(&mut x);
+    
+    println!("Here, x is {} and y is {}", x, y);
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
-
-    &s
+fn change_value(s : &mut String){
+    s.push_str(" world");
 }
+
+// fn main() {
+//     let _reference_to_nothing = dangle();
+// }
+
+// fn dangle() -> &String {
+//     let s = String::from("hello");
+
+//     &s
+// }
