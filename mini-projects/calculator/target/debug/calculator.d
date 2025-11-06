1d93452c84f0c7bb24a704f7b3cea4874b4165e2
Abhishek-B-R <byteblaster1468@gmail.com>
2025-11-06 20:25:38 +0530

    calculator application in rust

---
+++ mini-projects/calculator/target/debug/calculator.d
@@ -0,0 +1,1 @@
+/home/abhishek/Blockchain_Web3/100daysofcode/Rust/mini-projects/calculator/target/debug/calculator: /home/abhishek/Blockchain_Web3/100daysofcode/Rust/mini-projects/calculator/src/main.rs
---
+++ mini-projects/calculator/target/debug/deps/calculator-1c33c4c81cb85165.d
@@ -0,0 +1,5 @@
+/home/abhishek/Blockchain_Web3/100daysofcode/Rust/mini-projects/calculator/target/debug/deps/calculator-1c33c4c81cb85165.d: src/main.rs
+
+/home/abhishek/Blockchain_Web3/100daysofcode/Rust/mini-projects/calculator/target/debug/deps/calculator-1c33c4c81cb85165: src/main.rs
+
+src/main.rs:
